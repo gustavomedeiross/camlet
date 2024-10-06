03910c75713fa6035bd5312b65e5c9b8a6cec460
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2024-10-06 18:49:08 -0300

    WIP: Setup dream server

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,8 @@
+let () =
+  Dream.run ~port:42069
+  @@ Dream.logger
+  @@ Dream.router
+       [ Dream.get "/" (fun _ -> Dream.html "<html><body><h1>Hello!</h1></body></html>") ]
+;;
+
 let () = print_endline "Hello, World!"
