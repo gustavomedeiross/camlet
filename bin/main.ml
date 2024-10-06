659ce09fc083e391c281c2d245ad617294b8df04
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2024-10-06 19:17:59 -0300

    Setup Tyxml

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,13 @@
+let hello_page =
+  let open Tyxml.Html in
+  let page_title = title (txt "My Title") in
+  html (head page_title []) (body [ div [ h1 [ txt "Hello World :)" ] ] ])
+;;
+
+let html page_html = Dream.html (Format.asprintf "%a" (Tyxml.Html.pp ()) page_html)
+
 let () =
   Dream.run ~port:42069
   @@ Dream.logger
-  @@ Dream.router
-       [ Dream.get "/" (fun _ -> Dream.html "<html><body><h1>Hello!</h1></body></html>") ]
+  @@ Dream.router [ Dream.get "/" (fun _ -> html hello_page) ]
 ;;
-
-let () = print_endline "Hello, World!"
