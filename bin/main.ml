f918cadd8f8c76db1828c1727258136bb83cd4dc
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2024-10-08 20:56:43 -0300

    Move HTML to module

--- bin/main.ml
+++ bin/main.ml
@@ -1,40 +1,45 @@
-let html_template body_html =
-  let open Tyxml.Html in
-  let page_title = title (txt "Pix") in
-  html
-    (head page_title [])
-    (* [ script ~a:[ a_src (Xml.uri_of_string "https://cdn.tailwindcss.com") ] (txt "") ]) *)
-    (body body_html)
-;;
+module Page = struct
+  let html_template body_html =
+    let open Tyxml.Html in
+    let page_title = title (txt "Pix") in
+    html
+      (head page_title [])
+      (* [ script ~a:[ a_src (Xml.uri_of_string "https://cdn.tailwindcss.com") ] (txt "") ]) *)
+      (body body_html)
+  ;;
 
-let home_page =
-  let open Tyxml.Html in
-  let payments = [ "payment_1"; "payment_2"; "payment_3"; "payment_4" ] in
-  let list =
-    payments
-    |> List.map
-       @@ fun payment_id ->
-       li
-         [ a ~a:[ a_href (Format.sprintf "/payments/%s" payment_id) ] [ txt payment_id ] ]
-  in
-  html_template [ ul list ]
-;;
+  let home =
+    let open Tyxml.Html in
+    let payments = [ "payment_1"; "payment_2"; "payment_3"; "payment_4" ] in
+    let list =
+      payments
+      |> List.map
+         @@ fun payment_id ->
+         li
+           [ a ~a:[ a_href (Format.sprintf "/payments/%s" payment_id) ] [ txt payment_id ]
+           ]
+    in
+    html_template [ ul list ]
+  ;;
 
-let payment_page payment_id =
-  let open Tyxml.Html in
-  html_template [ div [ txt (Format.sprintf "Hello to %s!" payment_id) ] ]
-;;
+  let payment_detail payment_id =
+    let open Tyxml.Html in
+    html_template [ div [ txt (Format.sprintf "Hello to %s!" payment_id) ] ]
+  ;;
+end
 
-let html page_html = Dream.html (Format.asprintf "%a" (Tyxml.Html.pp ()) page_html)
+module View = struct
+  let html page_html = Dream.html (Format.asprintf "%a" (Tyxml.Html.pp ()) page_html)
+end
 
 let () =
   Dream.run ~port:42069
   @@ Dream.logger
   @@ Dream.router
-       [ Dream.get "/" (fun _ -> html home_page)
+       [ Dream.get "/" (fun _ -> View.html Page.home)
        ; (Dream.get "/payments/:payment_id"
           @@ fun request ->
           let payment_id = Dream.param request "payment_id" in
-          html (payment_page payment_id))
+          View.html (Page.payment_detail payment_id))
        ]
 ;;
