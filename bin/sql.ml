c87805f82809d864dcc05a6feb1bc47ae3caf55f
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2024-10-17 20:23:48 -0300

    Delete unused files

--- bin/sql.ml
+++
@@ -1,9 +0,0 @@
-module T = Caqti_type
-open Caqti_request.Infix
-open Caqti_type.Std
-
-let select_payments = (unit ->* tup2 string string) "SELECT id, created_at FROM payments"
-
-let select_payment =
-  (string ->! tup2 string string) "SELECT id, created_at FROM payments WHERE id = ?"
-;;
